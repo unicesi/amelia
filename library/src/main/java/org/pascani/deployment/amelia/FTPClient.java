b7387265ca9417a7ad5d9ed32fc9b04b38bb9bd0
jachinte <migueljimenezachinte@gmail.com>
2015-09-25 00:11:56 -0500

    :zap: Add method for creating several directories at once

--- library/src/main/java/org/pascani/deployment/amelia/FTPClient.java
+++ library/src/main/java/org/pascani/deployment/amelia/FTPClient.java
@@ -9,17 +9,45 @@ import org.apache.commons.net.ftp.FTP;
 
 public class FTPClient extends org.apache.commons.net.ftp.FTPClient {
 
+	@Override
+	public boolean makeDirectory(String remotePath) throws IOException {
+		mlistDir(remotePath);
+
+		// No such file or directory
+		if (getReplyCode() == 550 && remotePath.contains(File.separator)) {
+			String parent = getParent(remotePath);
+			makeDirectory(parent);
+		}
+
+		return super.makeDirectory(remotePath);
+	}
+
+	protected String getParent(String path) {
+		int last = path.length();
+
+		if (path.contains(File.separator))
+			last = path.lastIndexOf(File.separator);
+
+		String parent = path.substring(0, last);
+		return parent;
+	}
+
 	public boolean upload(String localPath, String remotePath)
 			throws IOException {
+
 		File file = new File(localPath);
-		boolean stored = false;
+		boolean dir = file.isDirectory();
+		boolean success = true;
 
-		if (file.isDirectory())
-			stored = uploadDirectory(localPath, remotePath);
+		// Makes sure the directory exists in the remote machine
+		makeDirectory(dir ? remotePath : getParent(remotePath));
+
+		if (dir)
+			success = uploadDirectory(localPath, remotePath);
 		else
-			stored = uploadFile(localPath, remotePath);
+			success = uploadFile(localPath, remotePath);
 
-		return stored;
+		return success;
 	}
 
 	private boolean uploadFile(String localPath, String remotePath)
@@ -41,12 +69,12 @@ public class FTPClient extends org.apache.commons.net.ftp.FTPClient {
 
 		File localDirectory = new File(localPath);
 		File[] subFiles = localDirectory.listFiles();
-		
+
 		remotePath = remotePath.isEmpty() ? "" : remotePath + "/";
 
 		if (subFiles != null && subFiles.length > 0) {
 			for (File file : subFiles) {
-				
+
 				String localFilePath = file.getAbsolutePath();
 				String remoteFilePath = remotePath + file.getName();
 
@@ -58,8 +86,9 @@ public class FTPClient extends org.apache.commons.net.ftp.FTPClient {
 				} else {
 					// create directory on the server
 					boolean created = makeDirectory(remoteFilePath);
-					success = created && uploadDirectory(localFilePath, remoteFilePath);
-							
+					success = created
+							&& uploadDirectory(localFilePath, remoteFilePath);
+
 					if (!success)
 						break;
 				}
