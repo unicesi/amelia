208d6e50e3fca690b85eb3e4401f661e9e0ec660
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-09-25 12:04:37 -0500

    :books: :sparkles: Add a method to remove directories with content

--- library/src/main/java/org/pascani/deployment/amelia/FTPClient.java
+++ library/src/main/java/org/pascani/deployment/amelia/FTPClient.java
@@ -6,10 +6,11 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPFile;
 
 /**
  * Wrapper implementation of the Apache commons net FTP client. In essence, this
- * class (re)implements:
+ * class implements:
  * 
  * <ul>
  * <li>{@link #makeDirectory(String)}: allows to create several directories at
@@ -23,22 +24,66 @@ import org.apache.commons.net.ftp.FTP;
  */
 public class FTPClient extends org.apache.commons.net.ftp.FTPClient {
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Creates new sub-directories on the FTP server in the current directory
+	 * (if a relative pathname is given) or where specified (if an absolute
+	 * pathname is given).
 	 * 
-	 * @see org.apache.commons.net.ftp.FTPClient#makeDirectory(java.lang.String)
+	 * @param pathname
+	 *            The pathname of the directory to create
+	 * @return whether the sub-directories were successfully created or not
+	 * @throws IOException
 	 */
-	@Override
-	public boolean makeDirectory(String remotePath) throws IOException {
-		mlistDir(remotePath);
+	public boolean makeDirectories(String pathname) throws IOException {
+		String s = remoteFileSeparator();
+		super.mlistDir(pathname);
 
 		// No such file or directory
-		if (getReplyCode() == 550 && remotePath.contains(File.separator)) {
-			String parent = getParent(remotePath);
-			makeDirectory(parent);
+		if (getReplyCode() == 550 && pathname.contains(s)) {
+			String parent = getPathParent(pathname);
+			makeDirectories(parent);
 		}
 
-		return super.makeDirectory(remotePath);
+		return super.makeDirectory(pathname);
+	}
+
+	/**
+	 * Removes a directory on the FTP server (with all of its contents)
+	 * 
+	 * @param pathname
+	 *            The pathname of the directory to delete
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean removeDirectoryWithContents(String pathname)
+			throws IOException {
+
+		boolean removed = true;
+		String s = remoteFileSeparator();
+
+		// List the remote files
+		FTPFile[] files = mlistDir(pathname);
+
+		if (files.length == 0)
+			removed = super.removeDirectory(pathname);
+		else {
+			// Remove all of the contents. files[0] is the directory itself
+			for (int i = 1; i < files.length && removed; i++) {
+				String name = files[i].getName();
+				if (name.equals(".") || name.equals(".."))
+					continue;
+
+				if (files[i].isDirectory())
+					removed = removeDirectoryWithContents(pathname + s + name);
+				else
+					removed = super.deleteFile(pathname + s + name);
+			}
+
+			// Remove the directory
+			super.removeDirectory(pathname);
+		}
+
+		return removed;
 	}
 
 	/**
@@ -58,18 +103,18 @@ public class FTPClient extends org.apache.commons.net.ftp.FTPClient {
 			throws IOException {
 
 		File file = new File(localPath);
-		boolean dir = file.isDirectory();
-		boolean success = true;
+		boolean isDir = file.isDirectory();
+		boolean uploaded = true;
 
-		// Makes sure the directory exists in the remote machine
-		makeDirectory(dir ? remotePath : getParent(remotePath));
+		// Makes sure the directories exists in the remote machine
+		makeDirectories(isDir ? remotePath : getPathParent(remotePath));
 
-		if (dir)
-			success = uploadDirectory(localPath, remotePath);
+		if (isDir)
+			uploaded = uploadDirectory(localPath, remotePath);
 		else
-			success = uploadFile(localPath, remotePath);
+			uploaded = uploadFile(localPath, remotePath);
 
-		return success;
+		return uploaded;
 	}
 
 	private boolean uploadFile(String localPath, String remotePath)
@@ -79,20 +124,22 @@ public class FTPClient extends org.apache.commons.net.ftp.FTPClient {
 
 		File localFile = new File(localPath);
 		InputStream stream = new FileInputStream(localFile);
-		boolean stored = storeFile(remotePath, stream);
+		boolean uploaded = storeFile(remotePath, stream);
 
 		stream.close();
-		return stored;
+		return uploaded;
 	}
 
 	private boolean uploadDirectory(String localPath, String remotePath)
 			throws IOException {
-		boolean success = true;
+
+		String separator = remoteFileSeparator();
+		boolean uploaded = true;
 
 		File localDirectory = new File(localPath);
 		File[] subFiles = localDirectory.listFiles();
 
-		remotePath = remotePath.isEmpty() ? "" : remotePath + "/";
+		remotePath = remotePath.isEmpty() ? "" : remotePath + separator;
 
 		if (subFiles != null && subFiles.length > 0) {
 			for (File file : subFiles) {
@@ -101,33 +148,55 @@ public class FTPClient extends org.apache.commons.net.ftp.FTPClient {
 				String remoteFilePath = remotePath + file.getName();
 
 				if (file.isFile()) {
-					success = uploadFile(localFilePath, remoteFilePath);
-					if (!success)
+					uploaded = uploadFile(localFilePath, remoteFilePath);
+					if (!uploaded)
 						break;
 
 				} else {
 					// create directory on the server
-					boolean created = makeDirectory(remoteFilePath);
-					success = created
+					boolean created = super.makeDirectory(remoteFilePath);
+					uploaded = created
 							&& uploadDirectory(localFilePath, remoteFilePath);
 
-					if (!success)
+					if (!uploaded)
 						break;
 				}
 			}
 		}
 
-		return success;
+		return uploaded;
 	}
 
-	private String getParent(String path) {
+	/**
+	 * @return The parent path of the specified path, based on the file
+	 *         separator
+	 * @throws IOException
+	 */
+	private String getPathParent(String path) throws IOException {
+		String separator = remoteFileSeparator();
 		int last = path.length();
 
-		if (path.contains(File.separator))
-			last = path.lastIndexOf(File.separator);
+		if (path.contains(separator))
+			last = path.lastIndexOf(separator);
 
 		String parent = path.substring(0, last);
 		return parent;
+	}
+
+	/**
+	 * By default, the file separator is UNIX like (/), unless the system type
+	 * specifies a Windows system.
+	 * 
+	 * @return The remote file separator
+	 */
+	public String remoteFileSeparator() throws IOException {
+		String separator = "/";
+		String systemType = super.getSystemType().toLowerCase();
+
+		if (systemType.contains("windows"))
+			separator = "\\";
+
+		return separator;
 	}
 
 }
