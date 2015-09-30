40dfa8a02605b4b1069cc5d112824cf0468010a3
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-09-30 16:52:59 -0500

    :books: Add class documentation

--- library/src/main/java/org/pascani/deployment/amelia/util/ShellUtils.java
+++ library/src/main/java/org/pascani/deployment/amelia/util/ShellUtils.java
@@ -1,38 +1,54 @@
 package org.pascani.deployment.amelia.util;
 
+/**
+ * This class encapsulates utility methods to work with shell interfaces
+ * 
+ * @author Miguel Jiménez - Initial contribution and API
+ */
 public class ShellUtils {
-	
+
 	/**
 	 * TODO: Only bash, zsh and dash are supported (not csh)
-	 *  
-	 * @return
+	 * 
+	 * @return The command to recognize the current shell being used by a UNIX
+	 *         machine
 	 */
 	public static String currentShellCommand() {
 		return "echo $0 | cut -c 2-";
 	}
-	
+
 	/**
+	 * Creates a string to set the prompt to a reliable prompt, that is, a
+	 * unique string not commonly returned by any process. By changing the
+	 * prompt, the probability of mistaken expected outputs is considerably
+	 * reduced.
+	 * 
 	 * TODO: Only bash and zsh are supported
 	 * 
 	 * @param shell
-	 * @return
+	 *            The current shell being used in the remote machine
+	 * @return a command setting the PS1 environment variable to the Amelia
+	 *         prompt
 	 */
 	public static String ameliaPromptFormat(String shell) {
-		
-		// [Amelia library 2015-09-28 18:54:34 user@grid0:~/Desktop]$ 
+
+		// e.g., [Amelia library 2015-09-28 18:54:34 user@grid0:~/Desktop]$
 		String bash = "[Amelia library \\D{%F %T} \\u@\\h:\\w]$ ";
 		String zsh = "[Amelia library %D{%Y-%m-%d %H:%M:%S} %n% @%m% :%~]$ ";
-		
+
 		String command = "PS1=\"";
-		
-		if(shell.contains("bash"))
+
+		if (shell.contains("bash"))
 			command += bash + "\"";
 		else
 			command += zsh + "\"";
-		
+
 		return command;
 	}
 
+	/**
+	 * @return the regular expression to recognize the Amelia prompt
+	 */
 	public static String ameliaPromptRegexp() {
 		String date = "(\\d+){4}-(\\d+){2}-(\\d+){2}";
 		String time = "(\\d+){2}:(\\d+){2}:(\\d+){2}";
