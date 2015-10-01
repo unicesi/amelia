72429d2dbbaf2aa96eaa7dff542d9a8852588d31
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-09-30 19:42:01 -0500

    :zap: Add generic deployment exception class

---
+++ library/src/main/java/org/pascani/deployment/amelia/util/DeploymentException.java
@@ -0,0 +1,14 @@
+package org.pascani.deployment.amelia.util;
+
+public class DeploymentException extends Exception {
+
+	/**
+	 * serial version UID
+	 */
+	private static final long serialVersionUID = 1794944839859341045L;
+	
+	public DeploymentException(String message) {
+		super(message);
+	}
+
+}
