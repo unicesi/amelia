ad4bd767eef8bd7e239f5d857586cd0f85517a9f
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-10-07 17:59:55 -0500

    :zap: Separate the FraSCAti-related tasks as callable classes

---
+++ library/src/main/java/org/pascani/deployment/amelia/process/Compilation.java
@@ -0,0 +1,40 @@
+package org.pascani.deployment.amelia.process;
+
+import static net.sf.expectit.matcher.Matchers.regexp;
+
+import java.util.concurrent.Callable;
+
+import org.pascani.deployment.amelia.descriptors.CompilationDescriptor;
+import org.pascani.deployment.amelia.util.DeploymentException;
+import org.pascani.deployment.amelia.util.ShellUtils;
+
+import net.sf.expectit.Expect;
+
+public class Compilation implements Callable<Boolean> {
+
+	private final Expect expect;
+
+	private final CompilationDescriptor descriptor;
+
+	public Compilation(final Expect expect, final CompilationDescriptor descriptor) {
+		this.expect = expect;
+		this.descriptor = descriptor;
+	}
+
+	public Boolean call() throws Exception {
+
+		// The Amelia prompt
+		String prompt = ShellUtils.ameliaPromptRegexp();
+
+		// Perform the compilation
+		this.expect.sendLine(descriptor.toCommandString());
+		String compile = this.expect.expect(regexp(prompt)).getBefore();
+
+		if (compile.contains("No such file or directory"))
+			throw new DeploymentException("No such file or directory \""
+					+ descriptor.sourceDirectory() + "\"");
+
+		return true;
+	}
+
+}
---
+++ library/src/main/java/org/pascani/deployment/amelia/process/Execution.java
@@ -0,0 +1,57 @@
+package org.pascani.deployment.amelia.process;
+
+import static net.sf.expectit.matcher.Matchers.regexp;
+
+import java.util.concurrent.Callable;
+
+import net.sf.expectit.Expect;
+import net.sf.expectit.ExpectIOException;
+
+import org.pascani.deployment.amelia.descriptors.ExecutionDescriptor;
+import org.pascani.deployment.amelia.util.DeploymentException;
+import org.pascani.deployment.amelia.util.ShellUtils;
+
+public class Execution implements Callable<Integer> {
+	
+	private final Expect expect;
+
+	private final ExecutionDescriptor descriptor;
+
+	public Execution(final Expect expect, final ExecutionDescriptor descriptor) {
+		this.expect = expect;
+		this.descriptor = descriptor;
+	}
+
+	public Integer call() throws Exception {
+		int PID = -1;
+
+		// The Amelia prompt
+		String prompt = ShellUtils.ameliaPromptRegexp();
+
+		// Send the run command
+		this.expect.sendLine(descriptor.toCommandString() + " &");
+		String _pid = this.expect.expect(regexp("\\[\\d+\\] (\\d+)")).group(1);
+
+		// Update the process ID
+		PID = Integer.parseInt(_pid);
+
+		try {
+			// Expect a successful execution
+			this.expect.expect(regexp("Press Ctrl\\+C to quit\\.\\.\\.|Call done!"));
+			this.expect.sendLine();
+			this.expect.expect(regexp(prompt));
+
+		} catch (ExpectIOException ex) {
+
+			// First, kill the remaining processes (if any)
+			String criterion = descriptor.toCommandSearchString();
+			this.expect.sendLine(ShellUtils.killCommand(criterion));
+
+			throw new DeploymentException(
+					"Cannot instantiate the FraSCAti factory!");
+		}
+
+		return PID;
+	}
+
+}
