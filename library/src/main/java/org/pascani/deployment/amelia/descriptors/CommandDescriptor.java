8d134c0e924f369ae759a6cce767d20614f220e8
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-10-08 11:19:07 -0500

    :zap: Turn the command definition into a concrete class representing any command (with limitations)

--- library/src/main/java/org/pascani/deployment/amelia/descriptors/CommandDescriptor.java
+++ library/src/main/java/org/pascani/deployment/amelia/descriptors/CommandDescriptor.java
@@ -2,8 +2,35 @@ package org.pascani.deployment.amelia.descriptors;
 
 import java.util.Observable;
 
-public abstract class CommandDescriptor extends Observable {
-	
-	public abstract String toCommandString();
+public class CommandDescriptor extends Observable {
 
+	private final String command;
+
+	private final String errorText;
+
+	private final String errorMessage;
+
+	public CommandDescriptor(final String command, final String errorText,
+			final String errorMessage) {
+		this.command = command;
+		this.errorText = errorText;
+		this.errorMessage = errorMessage;
+	}
+
+	public String toCommandString() {
+		return this.command;
+	}
+
+	@Override
+	public String toString() {
+		return this.command;
+	}
+
+	public String errorText() {
+		return this.errorText;
+	}
+
+	public String errorMessage() {
+		return this.errorMessage;
+	}
 }
