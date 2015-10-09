167451b823282a57a005e862b69edd1e66bb5d66
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-10-08 19:40:07 -0500

    :zap: Add a generic implementation for executing shell commands

--- library/src/main/java/org/pascani/deployment/amelia/process/Command.java
+++ library/src/main/java/org/pascani/deployment/amelia/process/Command.java
@@ -6,23 +6,33 @@ import java.util.concurrent.Callable;
 
 import org.pascani.deployment.amelia.DeploymentException;
 import org.pascani.deployment.amelia.descriptors.CommandDescriptor;
+import org.pascani.deployment.amelia.util.DependencyGraph;
 import org.pascani.deployment.amelia.util.ShellUtils;
 
 import net.sf.expectit.Expect;
 
+/**
+ * 
+ * New child classes need to add a case in:
+ * {@link DependencyGraph#addElement(java.util.Observable, SSHHandler)}
+ * 
+ * @see DependencyGraph#addElement(java.util.Observable, SSHHandler)
+ * 
+ * @author Miguel Jiménez - Initial contribution and API
+ */
 public class Command implements Callable<Boolean> {
 
 	private final Expect expect;
-	
+
 	private final CommandDescriptor descriptor;
-	
+
 	public Command(final Expect expect, final CommandDescriptor descriptor) {
 		this.expect = expect;
 		this.descriptor = descriptor;
 	}
 
 	public Boolean call() throws Exception {
-		
+
 		String prompt = ShellUtils.ameliaPromptRegexp();
 
 		this.expect.sendLine(this.descriptor.toCommandString());
@@ -30,7 +40,7 @@ public class Command implements Callable<Boolean> {
 
 		if (cd.contains(this.descriptor.errorText()))
 			throw new DeploymentException(this.descriptor.errorMessage());
-		
+
 		return true;
 	}
 
