df6a6887a11fc83ecc8aecd7e3e687c098bdcb01
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-10-13 12:32:47 -0500

    :lipstick: Apply code formatting

--- library/src/main/java/org/pascani/deployment/amelia/process/Run.java
+++ library/src/main/java/org/pascani/deployment/amelia/process/Run.java
@@ -1,6 +1,7 @@
 package org.pascani.deployment.amelia.process;
 
 import static net.sf.expectit.matcher.Matchers.regexp;
+import static org.pascani.deployment.amelia.util.Strings.ascii;
 
 import java.util.concurrent.Callable;
 
@@ -10,16 +11,17 @@ import net.sf.expectit.ExpectIOException;
 import org.pascani.deployment.amelia.DeploymentException;
 import org.pascani.deployment.amelia.descriptors.ExecutionDescriptor;
 import org.pascani.deployment.amelia.descriptors.Host;
+import org.pascani.deployment.amelia.util.Log;
 import org.pascani.deployment.amelia.util.ShellUtils;
 
 public class Run extends Command<Integer> implements Callable<Integer> {
-	
+
 	public Run(final Host host, final ExecutionDescriptor descriptor) {
 		super(host, descriptor);
 	}
 
 	public Integer call() throws Exception {
-		
+
 		int PID = -1;
 		ExecutionDescriptor descriptor = (ExecutionDescriptor) super.descriptor;
 		Expect expect = this.host.ssh().expect();
@@ -30,20 +32,23 @@ public class Run extends Command<Integer> implements Callable<Integer> {
 		// Send the run command
 		expect.sendLine(descriptor.toCommandString() + " &");
 		String _pid = expect.expect(regexp("\\[\\d+\\] (\\d+)")).group(1);
-		
+
 		// Detach the process
 		expect.sendLine(ShellUtils.detachProcess());
 
 		PID = Integer.parseInt(_pid);
 		try {
-			// Expect a successful execution
+			// Expect for a successful execution
 			expect.expect(regexp("Press Ctrl\\+C to quit\\.\\.\\.|Call done!"));
 			expect.sendLine();
 			expect.expect(regexp(prompt));
 
 		} catch (ExpectIOException ex) {
-			throw new DeploymentException(
-					"Cannot instantiate the FraSCAti factory!");
+			String message = "Cannot instantiate the FraSCAti factory!";
+
+			Log.info(super.host.toFixedString() + " " + ascii(10007) + " "
+					+ message);
+			throw new DeploymentException(message);
 		}
 
 		return PID;
