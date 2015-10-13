814bdb093b6a400107e6186985f0c564eba04a97
Miguel Jiménez <migueljimenezachinte@gmail.com>
2015-10-12 23:38:05 -0500

    :sparkles: Add a class to format output text with ANSI codes

---
+++ library/src/main/java/org/pascani/deployment/amelia/util/Log.java
@@ -0,0 +1,40 @@
+package org.pascani.deployment.amelia.util;
+
+import static org.pascani.deployment.amelia.util.Strings.ascii;
+
+public class Log {
+	
+	public static void heading(String message) {
+		message = "\n" + ascii(187) + " " + cyan(message);
+		System.out.println(message);
+	}
+	
+	public static void info(String message) {
+		message = message.replaceAll(ascii(10003), green(ascii(10003))); // ✓
+		message = message.replaceAll(ascii(10007), red(ascii(10007))); // ✗
+
+		System.out.println(message);
+	}
+	
+	public static void error(String message) {
+		message = red(message);
+		System.out.println(message);
+	}
+	
+	private static String red(String text) {
+		return "\u001b[1;31m" + text + reset();
+	}
+	
+	private static String green(String text) {
+		return "\u001b[1;32m" + text + reset();
+	}
+	
+	private static String cyan(String text) {
+		return "\u001b[1;36m" + text + reset();
+	}
+	
+	private static String reset() {
+		return "\u001b[0m";
+	}
+
+}
