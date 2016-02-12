20d2bea28a624583ba747e754b09c94b7fea2ce4
Miguel Jiménez <migueljimenezachinte@gmail.com>
2016-02-12 14:06:51 -0500

    Add a static method to concatenate string arrays

--- plugins/org.amelia.dsl.lib/src/main/java/org/amelia/dsl/lib/util/Strings.java
+++ plugins/org.amelia.dsl.lib/src/main/java/org/amelia/dsl/lib/util/Strings.java
@@ -26,6 +26,13 @@ import java.util.Collection;
  */
 public class Strings {
 	
+	public static String[] concatenate(String[] left, String[] right) {
+		String[] result = new String[left.length + right.length];
+		System.arraycopy(left, 0, result, 0, left.length);
+		System.arraycopy(right, 0, result, left.length, right.length);
+		return result;
+	}
+	
 	public static String[] skip(String[] strings, int elementsToSkip) {
 		if (elementsToSkip > strings.length)
 			throw new InvalidParameterException(
