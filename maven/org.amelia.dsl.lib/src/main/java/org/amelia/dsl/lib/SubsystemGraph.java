43d0b108efdb84b1ee8191e14ded2b9fc174a2f9
Miguel Jiménez <migueljimenezachinte@gmail.com>
2016-04-14 12:28:19 -0500

    Correct reset position to detect deployment errors correctly

--- maven/org.amelia.dsl.lib/src/main/java/org/amelia/dsl/lib/SubsystemGraph.java
+++ maven/org.amelia.dsl.lib/src/main/java/org/amelia/dsl/lib/SubsystemGraph.java
@@ -237,6 +237,7 @@ public class SubsystemGraph extends HashMap<Subsystem, List<Subsystem>> {
 			shutdown(stopExecutedComponents);
 			printExecutionSummary(start, System.nanoTime());
 			successful = !ExecutionManager.isAnySubsystemAborting();
+			ExecutionManager.reset();
 		} else {
 			shutdown(stopExecutedComponents);
 		}
@@ -274,7 +275,6 @@ public class SubsystemGraph extends HashMap<Subsystem, List<Subsystem>> {
 				}
 			}
 			instance = null;
-			ExecutionManager.reset();
 		}
 	}
 
