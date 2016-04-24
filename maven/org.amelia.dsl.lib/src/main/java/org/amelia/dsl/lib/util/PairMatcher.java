12c0eb8b8aa7dd96fb92eed2d4cf707ea1c7bb38
Miguel Jiménez <migueljimenezachinte@gmail.com>
2016-04-24 18:13:33 -0500

    Add documentation and file header

--- maven/org.amelia.dsl.lib/src/main/java/org/amelia/dsl/lib/util/PairMatcher.java
+++ maven/org.amelia.dsl.lib/src/main/java/org/amelia/dsl/lib/util/PairMatcher.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright © 2015 Universidad Icesi
+ * 
+ * This file is part of the Amelia library.
+ * 
+ * The Amelia library is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * The Amelia library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Amelia library. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.amelia.dsl.lib.util;
 
 import java.util.ArrayList;
@@ -8,6 +26,9 @@ import java.util.Stack;
 
 import com.google.common.collect.Range;
 
+/**
+ * @author Miguel Jiménez - Initial contribution and API
+ */
 public class PairMatcher {
 
 	private static Comparator<Range<Integer>> ascendent = new Comparator<Range<Integer>>() {
